a6c6498ff7f0c8b4dd890fe815a2eecece977f9f
bvrevanth <110579946+bvrevanth@users.noreply.github.com>
2023-02-11 21:57:58 +0530

    Routing - nested & dynamic
    learnt about link , createBrowserRouter,RouterProvider,Outlet and useRouteError , useParams hooks

---
+++ 07-finding-path/src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About us page</h1>
+      <p>THis is the namaste react course</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ 07-finding-path/src/components/Body.js
@@ -0,0 +1,81 @@
+import { restaurantList } from "../config";
+import RestaurantCard from "./RestaurantCard";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
+
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase())
+  );
+  return filterData;
+}
+
+const Body = () => {
+  const [allRestaurants, setAllRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState(" ");
+
+  useEffect(() => {
+    // API call
+    getRestaurants();
+  }, []);
+
+  async function getRestaurants() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.5061743&lng=80.6480153&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+
+    setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  }
+
+  if (!allRestaurants) return null;
+
+  // if (filteredRestaurants?.length === 0)
+  //   return <h1>No restaurant match your filter !</h1>;
+
+  return allRestaurants?.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(searchText, allRestaurants);
+            setFilteredRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {filteredRestaurants.map((restaurant) => {
+          return (
+            <Link
+              to={"/restaurant/" + restaurant.data.id}
+              key={restaurant.data.id}
+            >
+              <RestaurantCard {...restaurant.data} />
+            </Link>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Body;
