19b7bbcd1d85a21cecfacc10bd023e8d4bc5af02
bvrevanth <110579946+bvrevanth@users.noreply.github.com>
2023-02-24 16:34:29 +0530

    props drilling , context

---
+++ 11-data-new-oil/src/App.js
@@ -0,0 +1,80 @@
+import React, { lazy, Suspense, useState } from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Error from "./components/Error";
+import Contact from "./components/Contact";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import RestaurantMenu from "./components/RestaurantMenu";
+import Profile from "./components/profile";
+import Shimmer from "./components/Shimmer";
+import UserContext from "./utils/UserContext";
+
+// LazyLoading / DynamicImport / OnDemandLoading
+const Instamart = lazy(() => import("./components/Instamart"));
+// Never load dynamiclly load component inside another component
+
+const AppLayout = () => {
+  const [user, setUser] = useState({
+    name: "Revanth Venkat",
+    email: "support@namasterev.com",
+  });
+  return (
+    <UserContext.Provider
+      value={{
+        user: user,
+        setUser: setUser,
+      }}
+    >
+      <Header />
+      <Outlet />
+      <Footer />
+    </UserContext.Provider>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+        children: [
+          {
+            path: "profile",
+            element: <Profile />,
+          },
+        ],
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurant/:id",
+        element: <RestaurantMenu />,
+      },
+      {
+        path: "/instamart",
+        element: (
+          <Suspense fallback={Shimmer}>
+            <Instamart />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<RouterProvider router={appRouter} />);
---
+++ 11-data-new-oil/src/components/Header.js
@@ -0,0 +1,55 @@
+import { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import useOnline from "../utils/useOnline";
+import UserContext from "../utils/UserContext";
+
+const loggedInUser = () => {
+  return false;
+};
+
+const Title = () => (
+  <a href="/">
+    <img
+      className="h-28 p-2"
+      alt="logo"
+      src="https://lh5.googleusercontent.com/p/AF1QipOhHeCaQ6Xb6RVf3R_ZBTbDK4FIug_203rKsHLT"
+    />
+  </a>
+);
+
+const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const isOnline = useOnline();
+  const { user } = useContext(UserContext);
+  return (
+    <div className="flex justify-between bg-pink-50 shadow-lg">
+      <Title />
+      <div className="nav-items">
+        <ul className="flex py-10">
+          <li className="px-2">
+            <Link to="/">Home</Link>
+          </li>
+          <Link to="/about">
+            <li className="px-2">About</li>
+          </Link>
+          <li className="px-2">
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li className="px-2">Cart</li>
+          <li className="px-2">
+            <Link to="/instamart">Instamart</Link>
+          </li>
+        </ul>
+      </div>
+      <h1> {isOnline ? "Green" : "Red"}</h1>
+      <span className="p-10 font-bold text-red-900"> {user.name} </span>
+      {isLoggedIn ? (
+        <button onClick={() => setIsLoggedIn(false)}>Logout</button>
+      ) : (
+        <button onClick={() => setIsLoggedIn(true)}>Login</button>
+      )}
+    </div>
+  );
+};
+
+export default Header;
